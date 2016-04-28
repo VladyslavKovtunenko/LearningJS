12d7fd7726a9a0d943bafb111ca4e30b5c9692b7
VladyslavKovtunenko <kovtunenkovladyslav@gmail.com>
2016-04-28 15:05:04 +0300

    Changed XMLHttpRequest

--- XMLHttpRequest/request.js
+++ XMLHttpRequest/request.js
@@ -8,13 +8,24 @@ function load() {
 
     requestResult = JSON.parse(xhp.responseText);
     console.log(requestResult);
-    console.log(requestResult[0]);
 
     createList(requestResult);
 }
 
 function createList(object) {
-    for (var i = 0; i < 2 /*object.length*/; i++){
-        console.log(object.question);
+    for (var i = 0; i < object.length; i++){
+        var newItem = document.createElement('li');
+        var itemString = object[i].question + "<ul><li>" + object[i].url + "</li>" + "<li>" + object[i].published_at + "</li>";
+
+        if(object[i].choices.length){
+            itemString += "<li>Choices:<ul>";
+            for (var j = 0; j < object[i].choices.length; j++){
+                itemString += "<li>" + object[i].choices[j].choice + "</li>";
+            }
+            itemString += "</ul>";
+        }
+
+        newItem.innerHTML = itemString + "</ul>";
+        list.appendChild(newItem);
     }
 }
