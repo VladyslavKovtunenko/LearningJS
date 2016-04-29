8ab86eca76e7ccd9e47a8e37e9dc2493a9148215
VladyslavKovtunenko <kovtunenkovladyslav@gmail.com>
2016-04-29 09:52:18 +0300

    postGet (400 BAD REQUEST)

--- XMLHttpRequest/postGet.js
+++ XMLHttpRequest/postGet.js
@@ -2,25 +2,23 @@ function send() {
     var usrPost = new XMLHttpRequest();
     var usrGet = new XMLHttpRequest();
     var user ={};
-    
-    user.name = "john";
-    user.age = "24";
-    /*user.name = document.getElementById("user").toString();
-    user.age = document.getElementById("age").toString();*/
+
+    user.name = document.getElementById("user").value;
+    user.age = document.getElementById("age").value;
     
     var info = JSON.stringify(user);
+    console.log(info);
 
-    usrPost.open('POST', '/*куда?*/', true);
-    usrPost.setRequestHeader('Content-Type', 'text-plain');
+    usrPost.open('POST', 'http://polls.apiblueprint.org/questions', true);
+    usrPost.setRequestHeader("X-Requested-With", "XMLHttpRequest");  // 400 (BAD REQUEST)
     usrPost.send(info);
-
 
     usrPost.onreadystatechange = function (){
         if (usrPost.readyState != 4) return;
         alert('Post request');
     };
 
-    usrGet.open('GET', '/*откуда?*/', true);
+    usrGet.open('GET', 'http://polls.apiblueprint.org/questions', true);
     usrGet.send();
 
     usrGet.onreadystatechange = function() {
