9d93b1fc74a3388948f0b2cdeb5a4dd65947dbe4
VladyslavKovtunenko <kovtunenkovladyslav@gmail.com>
2016-04-29 12:33:59 +0300

    refactor

--- XMLHttpRequest/postAnswer.js
+++ XMLHttpRequest/postAnswer.js
@@ -19,14 +19,53 @@ function send(checked) {
     data.choices = checked;
     
     var info = JSON.stringify(data);
-    alert(info);
+    console.log(info);
 
     usrPost.open('POST', 'http://polls.apiblueprint.org/questions', true);
     usrPost.setRequestHeader("Content-Type", "application/json");
     usrPost.send(info);
-
-    usrPost.onreadystatechange = function (){
+  
+  /*usrPost.onreadystatechange = function (){
         if (usrPost.readyState != 4) alert('Post request');
-        /*alert('Post request');*/
-    };
+    };*/
+
+    setTimeout(function () {
+        load();
+    }, 1000);
+}
+
+function load() {
+    var getRequest = new XMLHttpRequest();
+    var requestResult;
+
+    getRequest.open('GET', 'http://polls.apiblueprint.org/questions', true);
+    getRequest.send();
+
+    getRequest.onreadystatechange = function() {
+        if (getRequest.readyState != 4) return;
+
+        requestResult = JSON.parse(getRequest.responseText);
+        console.log(requestResult);
+
+        createList(requestResult);
+    }
+}
+
+function createList(object) {
+    document.getElementById("list").innerHTML = "";
+    for (var i = 0; i < object.length; i++){
+        var newItem = document.createElement('li');
+        var itemString = object[i].question + "<ul><li>" + object[i].url + "</li>" + "<li>" + object[i].published_at + "</li>";
+
+        if(object[i].choices.length){
+            itemString += "<li>Choices:<ul>";
+            for (var j = 0; j < object[i].choices.length; j++){
+                itemString += "<li>" + object[i].choices[j].choice + "</li>";
+            }
+            itemString += "</ul>";
+        }
+
+        newItem.innerHTML = itemString + "</ul>";
+        list.appendChild(newItem);
+    }
 }
